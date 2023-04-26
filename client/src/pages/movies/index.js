4ee9d4127fd98887a3bdcd862ab7912157372b48
nicolasvillarr <nicolasvillar4@gmail.com>
2023-04-26 14:24:44 -0300

    first commit

---
+++ client/src/pages/movies/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+    <h1>estas van a ser las peliculas</h1>
+    <li></li>
+    </div>
+  )
+}
---
+++ client/src/pages/series/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+    <h1>estas van a ser las series</h1>
+    <li></li>
+    </div>
+  )
+}
